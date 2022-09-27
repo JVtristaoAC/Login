4f7c30021b640227319a486911d8cb0a391596d5
Portugal <jvtristaoac@gmail.com>
2022-09-27 12:27:24 -0300

    importação de imagens e inicio da estilização

--- App/config.php
+++ App/config.php
@@ -2,8 +2,8 @@
 define('BASEDIR', dirname(__FILE__, 2));
 define('VIEWS', BASEDIR . '/App/View/modules/');
 
-$_ENV['db']['host'] = 'localhost:3308';
+$_ENV['db']['host'] = 'localhost:3307';
 $_ENV['db']['user'] = 'root';
-$_ENV['db']['pass'] = '';
+$_ENV['db']['pass'] = 'etecjau';
 $_ENV['db']['database'] = 'db_mvc';
 ?>
